69653d3817ed3b403372211c175c88e7b5e926b7
Dzmitry.Shaplyka <dmitryshaplyko@gmail.com>
2017-07-17 12:39:57 +0300

    new confing added

---
+++ protractorMM.conf.js
@@ -0,0 +1,28 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    globalTimeout: 300000,
+    pageTimeout: 300000,
+    baseUrl: 'https://new.lamiabiblioteca.com/',
+    allScriptsTimeout: 300000,
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    specs: [
+        './test/e2e/features/*/*.feature'
+    ],
+    cucumberOpts: {
+        require: [
+            './test/e2e/step_definitions/*.js',
+            './env.js',
+            './hooks.js'
+        ],
+        format: 'pretty'
+    },
+
+    onPrepare: function () {
+        browser.manage().window().setSize(800, 600);
+        console.log('e2e started');
+    }
+};
